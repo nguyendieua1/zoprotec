2f3748b62438ab7789d27dbf1247648b442fc96c
nguyendieua1 <ctg.dieunx@gmail.com>
2022-07-25 14:24:24 +0700

    update component cart-checkout

---
+++ app/Http/Livewire/CheckoutConponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CheckoutConponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.checkout-conponent');
+    }
+}
