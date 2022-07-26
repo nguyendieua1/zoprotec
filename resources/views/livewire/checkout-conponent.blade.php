17bd29441dec845d27b7eabfca7573f99ad6f8ba
nguyendieua1 <ctg.dieunx@gmail.com>
2022-07-26 08:26:11 +0700

    update

--- resources/views/livewire/checkout-conponent.blade.php
+++ resources/views/livewire/checkout-conponent.blade.php
@@ -103,7 +103,7 @@
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="assets/images/products/digital_04.jpg" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+                                    <figure><img src="{{ asset('assets/images/products/digital_04.jpg') }}" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
                                 </a>
                                 <div class="group-flash">
                                     <span class="flash-item new-label">new</span>
