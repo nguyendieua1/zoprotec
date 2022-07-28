301db4124ecc262f427e124603fbf262ebb29b32
nguyendieu <ctg.dieunx@gmail.com>
2022-07-28 23:13:33 +0700

    big update

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::get('/cart',\App\Http\Livewire\CartComponent::class);
 Route::get('/checkout', \App\Http\Livewire\CheckoutConponent::class);
 Route::get('/about-us',\App\Http\Livewire\AboutusComponent::class);
 Route::get('/contact-us',\App\Http\Livewire\ContactusComponent::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
